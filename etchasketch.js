4173049f80648794159baf947825c9bf6948aef3
Barry <ballenwilliams@gmail.com>
2022-04-16 11:47:28 -0500

    Built skeleton css and js

---
+++ etchasketch.js
@@ -0,0 +1,11 @@
+const div = document.createElement('div');
+const gridbody = document.querySelector("#main");
+const container = document.createElement('div');
+
+container.textContent = "This is a DIV";
+//container.style.backgroundColor = "gray";
+
+gridbody.appendChild(container);
+
+
+
