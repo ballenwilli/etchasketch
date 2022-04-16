e1092849017b99ef01182a42c2319d128b664424
Barry <ballenwilliams@gmail.com>
2022-04-16 14:47:57 -0500

    Added boxes and mouseover code to js added new class to css

--- etchasketch.js
+++ etchasketch.js
@@ -2,10 +2,28 @@ const div = document.createElement('div');
 const gridbody = document.querySelector("#main");
 const container = document.createElement('div');
 
-container.textContent = "This is a DIV";
+let i = 0;
+container.textContent="A";
 
 
-gridbody.appendChild(container);
+for (i; i<=256; i++) {
+    addBox(i);    
+}
+
+function addBox(i){
+    const gridbox = document.createElement('div');
+    gridbox.className = "inner-box";
+    gridbox.textContent = i;
+    gridbox.id="stupid";    
+    gridbody.appendChild(gridbox);
+
+}
+document.getElementById("stupid").addEventListener("mouseover", function() {
+    document.getElementById("stupid").style.backgroundColor="black";
+});
+
+
+
 
 
 
