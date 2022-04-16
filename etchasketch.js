32b7e40c1ad6f01832ae9f8b0a0e0405046acb35
Barry <ballenwilliams@gmail.com>
2022-04-16 12:00:10 -0500

    cosmetic changes to boxes

--- etchasketch.js
+++ etchasketch.js
@@ -3,7 +3,7 @@ const gridbody = document.querySelector("#main");
 const container = document.createElement('div');
 
 container.textContent = "This is a DIV";
-//container.style.backgroundColor = "gray";
+
 
 gridbody.appendChild(container);
 
